b7a6ec1e2fc2e5490f12f4edb19418396be0e4ba
Harlan Howe <hhowe7@gatech.edu>
2021-11-18 10:41:33 -0600

    Added code to make a version that just draws a single Cell object, so that students can practice with the Cell in isolation.

--- src/GridDemoRunner.java
+++ src/GridDemoRunner.java
@@ -3,8 +3,13 @@ public class GridDemoRunner
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
-		GridDemoFrame app = new GridDemoFrame();
+
+		// by default, we're using the StartHereFrame so you can get used to the Cell class;
+		// switch the commenting between these lines when you are ready for the full program.
+
+		StartHereFrame practiceApp = new StartHereFrame();
+//		GridDemoFrame app = new GridDemoFrame();
+
 	}
 
 }
